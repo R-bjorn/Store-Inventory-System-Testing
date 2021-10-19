9441732429a81d43bf1d27ea6f03158b16c2b0c0
r-bjorn <ravidtrivedi111@gmail.com>
2021-10-19 18:58:23 -0400

    Testing Store Methods
    Adding Tests for methods CheckForItem, GetItemById and GetTotalSalesValue

--- src/StoreTest.java
+++ src/StoreTest.java
@@ -80,4 +80,57 @@ class TestStore {
 		assertEquals(1,store.getTotalNumberOfSales());
 		assertEquals(100, store.getTotalSalesValue());
 	}
+	
+	@Test
+	@Order(4)
+	void testCheckForItem() {
+		System.out.println("testCheckForItem called");
+		assertEquals(null, store.checkForItem(1234));
+		Item item = new Item("Title",10.00, LocalDate.of(1999, 11, 22),100);
+		store.addItem(item);
+//		store.listInventory();
+//		The new item is not null nor the same as the item
+//		It is a deep copy of the item in store inventory
+		assertNotEquals(null, store.checkForItem(9200));
+		assertNotEquals(item, store.checkForItem(9200));
+	}
+
+	@Test
+	@Order(5)
+	void testGetItemByID() {
+		System.out.println("testCheckForItem called");
+		assertEquals(null, store.checkForItem(1234));
+		Item item = new Item("Title",10.00, LocalDate.of(1999, 11, 22),100);
+		store.addItem(item);
+//		Same item returns
+		assertEquals(item, store.getItemByID(9300));
+	}
+
+	@Test
+	@Order(5)
+	void testGetTotalSalesValue() {
+		System.out.println("testGetTotalSalesValue called");
+		store.addItem(new Item("Title",10.00, LocalDate.of(1999, 11, 22),100));
+		store.makeSale(9400, 10, LocalDate.of(2000, 11, 22));
+		System.out.println("--------------------------------");
+		store.listInventory();
+		assertEquals(100, store.getTotalSalesValue());
+		store.makeSale(9400, 10, LocalDate.of(2000, 11, 22));
+		assertEquals(200, store.getTotalSalesValue());
+		store.makeSale(9400, 10, LocalDate.of(2000, 11, 22));
+		assertEquals(300, store.getTotalSalesValue());
+	}
+
+	@Test
+	@Order(6)
+	void testGetTotalNumberOfSales() {
+		System.out.println("testGetTotalNumberOfSales called");
+		store.addItem(new Item("Title",10.00, LocalDate.of(1999, 11, 22),100));
+		store.makeSale(9500, 10, LocalDate.of(2000, 11, 22));
+		assertEquals(1, store.getTotalNumberOfSales());
+		store.makeSale(9500, 10, LocalDate.of(2000, 11, 22));
+		assertEquals(2, store.getTotalNumberOfSales());
+		store.makeSale(9500, 10, LocalDate.of(2000, 11, 22));
+		assertEquals(3, store.getTotalNumberOfSales());
+	}
 }
